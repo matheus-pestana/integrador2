f335da3632ca7436fe067bf076d8fbc3548e4ce8
Julio César Morais <juliocesarmorais78@gmail.com>
2025-10-16 15:36:29 +0000

    altere a posição do "Data Treatment" para ficar do lado esquerdo do camp

--- src/app/(app)/segmentation/page.tsx
+++ src/app/(app)/segmentation/page.tsx
@@ -85,7 +85,7 @@ export default function SegmentationPage() {
             </div>
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
-                <div className="lg:col-span-1 space-y-8">
+                <div className="lg:col-span-1 grid grid-cols-1 gap-8 items-start">
                     <Card>
                         <CardHeader>
                             <CardTitle>Data Treatment</CardTitle>
